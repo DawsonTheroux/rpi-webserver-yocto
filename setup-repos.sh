820e2a648deaee9265f70edb552f263ac1ff6262
Dawson Theroux <dawsontheroux@gmail.com>
2024-10-03 10:04:06 -0400

    Fixed setup-repos.sh to clone repos and added README.md

--- setup-repos.sh
+++ setup-repos.sh
@@ -4,10 +4,10 @@ mkdir sources
 cd sources
 
 echo && echo "Cloning poky..." && echo
- 1878  git clone https://github.com/yoctoproject/poky.git -b scarthgap
+git clone https://github.com/yoctoproject/poky.git -b scarthgap
 
 echo && echo "Cloning meta-openembedded..." && echo
- 1880  git clone https://github.com/openembedded/meta-openembedded.git -b scarthgap
+git clone https://github.com/openembedded/meta-openembedded.git -b scarthgap
 
 echo && echo "Cloning meta-raspberrypi..." && echo
- 1884  git clone https://github.com/agherzan/meta-raspberrypi.git -b scarthgap
+git clone https://github.com/agherzan/meta-raspberrypi.git -b scarthgap
